8149e7ae8f830432197973e0fc5b03fad3e3e90e
CCAC_Loaner <lidanyang1111@gmail.com>
2022-05-11 20:59:18 -0400

    Add project files.

---
+++ MovieShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MovieShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
