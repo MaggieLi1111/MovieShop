4d2ccb9ebbaf838d917e75739382992b5cb331c4
Maggie <lidanyang1111@gmail.com>
2022-05-19 10:48:37 -0400

    add movie repository pagination

--- ApplicationCore/Contracts/Repositories/IMovieRepository.cs
+++ ApplicationCore/Contracts/Repositories/IMovieRepository.cs
@@ -1,4 +1,5 @@
 ﻿using ApplicationCore.Entities;
+using ApplicationCore.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,9 @@ namespace ApplicationCore.Contracts.Repositories
     public interface IMovieRepository: IRepository<Movie>
     {
         Task<List<Movie>> GetTop30GrossingMovies();
-        Task<> GetMoviesByGenres(int genreId, int pageSize = 30, int pageNumber = 1);
+
+        
+        Task<PagedResultSet<Movie>> GetMoviesByGenres(int genreId, int pageSize = 30, int pageNumber = 1);
        
     } 
 }
--- Infrastructure/Repositories/MovieRepository.cs
+++ Infrastructure/Repositories/MovieRepository.cs
@@ -7,10 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using ApplicationCore.Models;
 
 namespace Infrastructure.Repositories
 {
-    public class MovieRepository: Repository<Movie>, IMovieRepository
+    public class MovieRepository : Repository<Movie>, IMovieRepository
     {
         public MovieRepository(MovieShopDbContext dbContext) : base(dbContext)
         {
@@ -21,14 +22,40 @@ namespace Infrastructure.Repositories
             var movies = await _dbContext.Movies.OrderByDescending(m => m.Revenue).Take(30).ToListAsync();
             return movies;
         }
+
         public override async Task<Movie> GetById(int id)
         {
-            var movieDetails = await _dbContext.Movies.Include(m=> m.MoviesOfGenre).ThenInclude(m => m.Genre)
-                .Include(m=> m.MoviesCasts)
+            var movieDetails = await _dbContext.Movies.Include(m => m.MoviesOfGenre).ThenInclude(m => m.Genre)
+                .Include(m => m.MoviesCasts)
                 .ThenInclude(m => m.Cast)
                 .Include(m => m.Trailers)
                 .FirstOrDefaultAsync(m => m.Id == id);
             return movieDetails;
+        }
+
+        public async Task<PagedResultSet<Movie>> GetMoviesByGenres(int genreId, int pageSize = 30, int pageNumber = 1)
+        {
+            var totalMoviesCountByGenre = await _dbContext.MovieGenres.Where(m => m.GenreId == genreId).CountAsync();
+
+            if (totalMoviesCountByGenre == 0)
+            {
+                throw new Exception("No Movies found for that genre");
+            }
+
+            var movies = await _dbContext.MovieGenres.Where(g => g.GenreId == genreId).Include(m => m.Movie)
+                .OrderBy(m => m.MovieId)
+                .Select(movie => new Movie
+                 {
+                    Id = movie.MovieId,
+                    PosterUrl = movie.Movie.PosterUrl,
+                    Title = movie.Movie.Title,
+                 })
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            var pagedMovies = new PagedResultSet<Movie>(movies, pageNumber, pageSize, totalMoviesCountByGenre);
+            return pagedMovies;
         }
     }
 }
